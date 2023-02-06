e929c543056c915a2a45a9d8ee3ce2a846eada03
CALVINCE <calvince.ogwel@gmail.com>
2023-02-06 06:36:40 +0300

    completed methods compulsory lab

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ function greetProgrammer() {
   console.log("Hello, programmer!");
 }
 
+
 /*
   You should be able to call this function with one argument and see its output in the terminal:
   greet("Naureen");
@@ -20,7 +21,7 @@ function greet(name) {
   You should be able to call this function with no arguments and see its output in the terminal:
   greetWithDefault();
   => "Hello, programmer!"
-  
+
   You should also be able to call this function with one argument and see its output in the terminal:
   greetWithDefault("Sunny");
   => "Hello, Sunny!"
--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+name="Naureen"
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="Naureen")
+    if name
+    puts "Hello, #{name}!"
+    end
+    puts "Hello, programmer!c"
+end
+
+def add num1, num2
+    return num1+num2
+end
+
+def halve(number)
+    if number != 6
+        return nil
+    end
+    return 6/2
+end
